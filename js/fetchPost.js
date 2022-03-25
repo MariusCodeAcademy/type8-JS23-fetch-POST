37f14362bf69988ee79d04782c14c6641b0e885d
Marius <marius@example.com>
2022-03-25 09:12:48 +0200

    post form

---
+++ js/fetchPost.js
@@ -0,0 +1,17 @@
+console.log('fetch post');
+
+const newPost = {
+  title: '',
+  body: '',
+  userId: '',
+};
+
+// 1. nusitaikyti i forma
+
+// 2. uzdeti submit event listeneri
+
+// 3. sustabdyti formos siuntima su perkrovimu
+
+// 4. surinkit inputu reiksmes ir sudeti i objekta
+
+// 5. siusti naujo post duomenis su fetch
